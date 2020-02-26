497b69d716d41b8226d3442f231cc13ce4a5321e
Kee <tkeeching@gmail.com>
2020-02-26 17:29:03 +1100

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,20 +4,22 @@ console.log("Up and running");
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0]);
-
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
+        console.log("You found a match!");
     } else {
-        alert("Sorry, try again.");
+        console.log("Sorry, try again.");
     }
 }
+
+function flipCard(cardId) {
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
+}
+
+flipCard(0);
+flipCard(2);
 
